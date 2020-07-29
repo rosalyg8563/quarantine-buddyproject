9c9ee2645c3fbbbfb55ff5af619e9097b47a5dfe
sus116 <nguyensusan989@gmail.com>
2020-07-29 23:54:06 +0000

    change

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
 
 
 # -- Initialization section --
@@ -13,4 +13,34 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template ("index.html")
+
+@app.route('/happy')
+def happy():
+    return render_template("happy.html")
+
+@app.route('/bored')
+def bored():
+    return render_template("bored.html")
+
+@app.route('/sad')
+def sad():
+    return render_template("sad.html")
+
+@app.route('/angry')
+def angry():
+    return render_template("angry.html")
+
+@app.route('/mission')
+def mission():
+    return render_template("mission.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/home')
+def home():
+    return render_template("index.html")
+
+
